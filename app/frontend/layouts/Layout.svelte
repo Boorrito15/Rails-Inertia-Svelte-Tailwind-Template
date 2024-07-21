0b3b4e03f8cba830022f3c7362d3f4a43b9316bb
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-22 00:39:27 +0700

    feat: add dark mode toggle to navbar
    Implemented a toggle switch in the navbar to allow users to switch between light and dark modes. Integrated the toggle with the existing dark mode functionality.

--- app/frontend/layouts/Layout.svelte
+++ app/frontend/layouts/Layout.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { ModeWatcher } from "mode-watcher";
-  import { Button } from "$lib/components/ui/button";
-  import Sun from "svelte-radix/Sun.svelte";
-  import Moon from "svelte-radix/Moon.svelte";
-  import { toggleMode } from 'mode-watcher';
   import MainNav from "../components/Layouts/MainNav.svelte";
 
   //   // Force light mode on initial load
@@ -15,18 +11,6 @@
 </script>
 
 <ModeWatcher defaultMode="light" />
-
-<div class="p-12">
-  <Button on:click={toggleMode} variant="outline" size="icon">
-    <Sun
-      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-    />
-    <Moon
-      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-    />
-    <span class="sr-only">Toggle theme</span>
-  </Button>
-</div>
 
 <div>
   <MainNav />
--- app/frontend/pages/events/index.svelte
+++ app/frontend/pages/events/index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <h1 class="text-3xl font-bold underline">Hello World!</h1>
-<p class="font-thin text-gray-700">This is the App component</p>
+<p class="font-thin">This is the App component</p>
 
 <h1>All Events</h1>
 
