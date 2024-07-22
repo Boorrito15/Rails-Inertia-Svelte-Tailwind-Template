cd25e8b78cd5ff32d981e673954dd90605550755
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-22 19:36:13 +0700

    Update the sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  # devise routes
+  devise_for :users, skip: [ :sessions, :passwords, :registrations ]
+  as :user do
+    get 'login', to: 'users/sessions#new', as: :new_user_session
+    post 'login', to: 'users/sessions#create', as: :user_session
+    match 'logout', to: 'users/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+  end
+
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
