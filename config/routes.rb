4d01eaf6a4ba6915c6b0c9491db8f3bd0261886a
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-22 21:19:31 +0700

    Generate the custom Devise controller for registrations:

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     get 'login', to: 'users/sessions#new', as: :new_user_session
     post 'login', to: 'users/sessions#create', as: :user_session
     match 'logout', to: 'users/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+
+    get 'sign_up', to: 'users/registrations#new', as: :new_user_registration
+    post 'sign_up', to: 'users/registrations#create', as: :user_registration
   end
 
   resources :events
