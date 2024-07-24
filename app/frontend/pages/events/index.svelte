47fa8431a35c700cb8c36468d4422a3fc6d7dce3
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-07-24 15:19:37 +0700

    feat: add inline event creation
    Updated the events index view to support inline creation of new events. Added a form that allows users to create an event without navigating away from the page. Includes JavaScript enhancements for handling form submission and user feedback.

--- app/frontend/pages/events/index.svelte
+++ app/frontend/pages/events/index.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
+  import New from './new.svelte'
   import { Link } from '@inertiajs/svelte';
+
+  let show = false
+
+  function toggleShow() {
+    show = !show
+  }
 
   export let events;
 
   // Reactively update `updatedEvents` when `events` changes
   $: updatedEvents = events;
+
 </script>
 
 <h1 class="mb-6 text-2xl font-bold">All events</h1>
 
-<Link href="/events/new">New event</Link>
+<Link href="/events/new">New event (New Page)</Link>
+
+<button on:click={ toggleShow } >New Event (In line)</button>
+
+{#if show}
+  <New/>
+{/if}
 
 <div class="mt-6 space-y-6">
   {#each updatedEvents as event (event.id)}
